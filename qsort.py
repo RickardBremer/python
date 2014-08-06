800625368ce350430109c41419161f0613b85461
punkiq <punkiq130@gmail.com>
2014-08-06 18:31:29 +0200

    opt and main

--- qsort.py
+++ qsort.py
@@ -12,23 +12,27 @@ def qsort(qslist):
 	if (len(qslist) < 2):
 		return qslist
 
-	else:
-		pivot = random.choice(qslist)
 
-		for x in qslist:
-			if x <= pivot: 
-				small.append(x)
-			elif x > pivot:
-				big.append(x)
+	pivot = random.choice(qslist)
+
+	for x in qslist:
+		if x <= pivot: 
+			small.append(x)
+		elif x > pivot:
+			big.append(x)
 			
-		if(len(big) == 0):
-			big.append(pivot)
-			small.remove(pivot)
+	if(len(big) == 0):
+		big.append(pivot)
+		small.remove(pivot)
 
 	return qsort(small) + qsort(big) 
 
-qslist = [9,1,8,2,7,3,6,4,5]
 
-print(qsort(qslist))
+def main():
 
+	qslist = [9,1,1,2,3,2,7,3,6,4,5] * 200
+	print(qsort(qslist))
+
+if __name__ == "__main__":
+	main()
 
