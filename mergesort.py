0b747943ef3228c4f8eeed26b24bb4f3a7ffd92b
punkiq <punkiq130@gmail.com>
2014-08-07 16:54:38 +0200

    merge.....sort

--- mergesort.py
+++ mergesort.py
@@ -7,8 +7,10 @@ def merge_sort(mslist):
 	if len(mslist) <= 1:
 		return mslist
 
-	left = mslist[:len(mslist)//2]
-	right = mslist[len(mslist)//2:]
+	middle = len(mslist)//2
+
+	left = mslist[:middle]
+	right = mslist[middle:]
 	
 	left = merge_sort(left)
 	right = merge_sort(right)
@@ -19,22 +21,28 @@ def merge(left,right):
 
 	result = []
 
-	while len(left) > 0 or len(right) > 0:
-		if len(left) > 0 and len(right) > 0:
+	leftlen = len(left)
+	rightlen = len(right)
+
+	while leftlen > 0 or rightlen > 0:
+		if leftlen > 0 and rightlen > 0:
 			if left[0] <= right[0]:
 				result.append(left.pop(0))
 			else:
 				result.append(right.pop(0))
-		elif len(left) > 0:
+		elif leftlen > 0:
 			result.append(left.pop(0))
-		elif len(right) > 0:
+		elif rightlen > 0:
 			result.append(right.pop(0))
+		
+		leftlen = len(left)
+		rightlen = len(right)	
 
 	return result
 
 def main():
 
-	mslist = [-9,1,1,2,3,2,7,3,6,4,5,4,3,5,6,7,8,9,0,0] * 102
+	mslist = [-9,1,1,2,3,2,7,3,6,4,5,4,3,5,6,7,8,9,0,0] * 10
 	print(merge_sort(mslist))
 
 if __name__ == "__main__":
